02aac693622636cbd6fe2c784c046daa733d72dd
manazarali01-hub <manazarali01@gmail.com>
2026-09-24 22:34:37 +0500

    Expose detailed bilingual recipe data

--- recipes-data.js
+++ recipes-data.js
@@ -4,7 +4,9 @@ window.PAKISTAN_FOOD_RECIPES = [
 {% for recipe_pair in site.data.recipes %}
   {% assign urdu_name = recipe_pair[1].name_urdu %}
   {% unless urdu_name %}{% assign urdu_name = site.data.urdu_names[recipe_pair[0]] %}{% endunless %}
-  Object.assign({{ recipe_pair[1] | jsonify }}, { slug: {{ recipe_pair[0] | jsonify }}, nameUrdu: {{ urdu_name | jsonify }} }){% unless forloop.last %},{% endunless %}
+  {% assign bilingual_category_key = recipe_pair[1].category | downcase %}
+  {% assign bilingual_recipe = site.data.bilingual[bilingual_category_key][recipe_pair[0]] %}
+  Object.assign({{ recipe_pair[1] | jsonify }}, { slug: {{ recipe_pair[0] | jsonify }}, nameUrdu: {{ urdu_name | jsonify }}, bilingual: {{ bilingual_recipe | jsonify }} }){% unless forloop.last %},{% endunless %}
 {% endfor %}
 ];
 
